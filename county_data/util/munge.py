eb462bfff49202e1f4536f676bb68cd9ada4e280
Erin <Erin-Boehmer@users.noreply.github.com>
2020-03-28 01:02:30 -0400

    Add rollup county case comparison and county automate data pulls (#5)
    * Add rollup county case comparison and automte data pulls
    
    * Add log scale comparison and remove unassigned values
    
    * Update x-axis label

---
+++ county_data/util/munge.py
@@ -0,0 +1,79 @@
+from IPython.display import display, HTML
+import pandas as pd
+
+
+state_abbrevs = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands':'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+}
+
+state_full_names = dict([(value, key) for key, value in state_abbrevs.items()])
+
+palette = {
+    "pink":"#DB3069",
+    "grey":"#313638",
+    "gold":"#F5D547",
+    "aqua":"#C1FFE8",
+    "royal":"#1446A0",
+    "fox":"#F06543"
+}
+
+def max_print(df):
+    """Print the entire contents of a dataframe"""
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+         display(HTML(df.to_html()))
+
